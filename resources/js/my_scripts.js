b0f8c66f3918489779c68580f22265b261f4dc3f
Ryan <ryho4913@gmail.com>
2019-10-04 16:24:00 -0600

    Nice Commit

--- resources/js/my_scripts.js
+++ resources/js/my_scripts.js
@@ -22,74 +22,114 @@ var players = [{name:"John Doe", img: "../resources/img/player1.jpg", alt:"Image
 		viewStudentStats(id, toggle) method
 			parameters:
 				id - The css id of the html tag being updated.
-				toggle - 
+				toggle -
 					0 - hide the html tag
 					1 - make the html tag visible
-			
+
 			purpose: This method will accept the id of an html tag and a toggle value.
-					 The method will then set the html tag's css visibility and height.  
+					 The method will then set the html tag's css visibility and height.
 					 To hide the html tag (toggle - 0), the visibility will be set to hidden and
-					 the height will be set to 0.  
+					 the height will be set to 0.
 					 To reveal the html tag (toggle - 1), the visibility will be set to visible and
 					 the height will be set to auto.
 */
-				
+function viewStudentStats(id, toggle){
+	if(toggle == "1"){
+		document.getElementById(id).style.height = "auto";
+		document.getElementById(id).style.visibility = "visible";
+	}else{
+		document.getElementById(id).style.height = "auto";
+		document.getElementById(id).style.visibility = "hidden";
+		document.getElementById("undergrad_select").style.height = "auto";
+		document.getElementById("undergrad_select").style.visibility = "hidden";
+		document.getElementById("grad_select").style.height = "auto";
+		document.getElementById("grad_select").style.visibility = "hidden";
+	}
+}
+
 /*
-	Home Page: 
+	Home Page:
 		changeColor(color) method
-			parameter: 
+			parameter:
 				color- A css color
-				
-			purpose: This method will set the html body's background color to the 
+
+			purpose: This method will set the html body's background color to the
 					 provided parameter.
 */
+function changeColor(color){
+	document.body.style.backgroundColor = color
+}
 
 
 /*
 	Football Season Stats Page:
 		loadStatsPage method:
 			parameters: none
-			
-			purpose: This method will iterate through the stats table and 
+
+			purpose: This method will iterate through the stats table and
 					 do the following:
 						1. Read through each row of the table & determine which team won
 						   the game.
-						
+
 						2. Update the winner column to the name of the winning team.
-						
+
 						3. Keep track of the number of wins/losses for the Buffs.
-						
+
 						4. Update the second table to show the total number of wins/losses for the Buffs.
 */
+function loadStatsPage(){
+	var table = document.getElementById("stats_table");
+	var rowC;
+	var columnC;
+	var cellVal;
+	var wins = 0;
+	var losses = 0;
+	for(rowC = 2; rowC < table.rows.length; rowC++){
+		if(parseInt(table.rows[rowC].cells[2].innerHTML) > parseInt(table.rows[rowC].cells[3].innerHTML))
+		{
+			table.rows[rowC].cells[4].innerHTML = "CU Boulder";
+			wins += 1;
+		}else{
+			table.rows[rowC].cells[4].innerHTML = table.rows[rowC].cells[1].innerHTML;
+			losses += 1;
+		}
+	}
+	document.getElementById("wins").innerHTML = wins;
+	document.getElementById("losses").innerHTML = losses;
+}
 
 /*
 	Football Player Information Page
 		loadPlayersPage method:
 			parameters: none
-			
-			purpose: This method will populate the dropdown menu to allow the 
+
+			purpose: This method will populate the dropdown menu to allow the
 					 user to select which player's information to view.
-					 
+
 					 To handle this, you will need to iterate through the players array
 					 and do the following for each player:
 						1. Create an anchor tag
-						2. Set the href to "#", this will make sure the 
+						2. Set the href to "#", this will make sure the
 							anchor tag doesn't change pages
-						3. Set the onclick to call switchPlayers method 
+						3. Set the onclick to call switchPlayers method
 							(this will need to pass in the index inside the players array)
-						4. Set the anchor tag's text to the player's name.
-						
+						4. Set the anvarchor tag's text to the player's name.
+
 					After setting all of the anchor tags, update the innerHTML of the dropdown menu.
 					As a note, the id for the dropdown menu is player_selector.
-		
+*/
+function loadPlayersPage(){
+
+}
+/*
 		switchPlayers(playerNum) method:
-			parameters: 
+			parameters:
 				playerNum - The index of the football player in the players array.
-			
+
 			purpose:
 				This method will update the the spans on the player's information pageX
 				and calculate the average passing, rushing, and receiving yards.
-				
+
 				Span ids:
 					p_year     - the player's year in college
 					p_major    - the player's major in college
@@ -98,11 +138,12 @@ var players = [{name:"John Doe", img: "../resources/img/player1.jpg", alt:"Image
 					p_yards    - the number of passing yards
 					r_yards    - the number of rushing yards
 					rec_yards  - the number of receiving yards
-					
+
 					Calculated values:
 					  avg_p_yards   - the average number of passing yards for the player's Buff career
 					  avg_r_yards   - the average number of rushing yards for the player's Buff career
 					  avg_rec_yards - the average number of receiving yards for the player's Buff career
 */
-				
-
+function switchPlayers(plaeyerNum){
+	
+}
